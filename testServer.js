8165cf8a616d31ddf64f5ae645de206312c72c29
Md.Sakibul Islam <83388440+isakibul15@users.noreply.github.com>
2022-09-07 14:08:30 +0600

    Create testServer.js
    Testing Branch Functionality

---
+++ testServer.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var server = http.createServer((req, res) => {
+//  console.log(req.method + " " + req.url + " " + req.httpVersion);
+
+  res.writeHead(200, {"Content-Type": "text/html"});
+
+  res.write("<h1> Hello World </h1>");
+  res.end();
+});
+
+server.listen(8000);
