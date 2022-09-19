fdb4b75c8d9fc5c3abd81d23ef7a1bddb2bb1643
isakibul15 <sakibulislam55@iut-dhaka.edu>
2022-09-19 15:07:10 +0600

    added hi

--- testServer.js
+++ testServer.js
@@ -5,7 +5,7 @@ var server = http.createServer((req, res) => {
 
   res.writeHead(200, {"Content-Type": "text/html"});
 
-  res.write("<h1> Hello World</h1>");
+  res.write("<h1> Hi World</h1>");
   res.end();
 });
 
